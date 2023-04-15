018720dfd7ff4a668520502e9781b4d35fb4a5b3
Manxi Wang <mxwang0102@gmail.com>
2023-04-14 22:12:24 -0700

    loan calculator complete

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,36 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  document.getElementById("loan-amount").value = 10000;
+  document.getElementById("loan-years").value = 5;
+  document.getElementById("loan-rate").value = 0.08;
+  let values = {
+    amount: 10000,
+    years: 5,
+    rate: 0.08,
+  };
+  update();
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let values = getCurrentUIValues();
+  let monthly = calculateMonthlyPayment(values);
+  updateMonthly(monthly);
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  let result = (Number(values.amount)*Number(values.rate)/1200)/(1-(1+Number(values.rate)/1200)**(-Number(values.years)*12));
+  return result.toFixed(2).toString();
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
+
 function updateMonthly(monthly) {
+  document.getElementById("monthly-payment").innerText=monthly;
 }
